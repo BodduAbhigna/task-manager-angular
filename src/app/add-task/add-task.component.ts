a39b75ca5553d8c7d8866dd4ba481e162f458693
Gabrijela <josipovic1501@gmail.com>
2024-01-29 14:44:30 +0100

    remove placeholder components, add page views with styling

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
--- src/app/edit-task/edit-task.component.ts
+++ src/app/edit-task/edit-task.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-edit-task',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule],
   templateUrl: './edit-task.component.html',
   styleUrl: './edit-task.component.css'
 })
